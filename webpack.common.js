ebc44ae80b2c37dca4541433f553e084ddae4b51
ivalynx <inkybasik@gmail.com>
2023-03-22 17:46:36 +0200

    fix to load img

--- webpack.common.js
+++ webpack.common.js
@@ -1,10 +1,15 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   output: {
     filename: 'app.bundle.js',
     path: path.resolve(__dirname, 'docs'),
+    assetModuleFilename: pathData => {
+      const filepath = path.dirname(pathData.filename).split('/').slice(1).join('/');
+      return `${filepath}/[name][ext]`;
+    },
   },
   module: {
     rules: [
@@ -40,6 +45,12 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html',
       filename: './index.html',
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: "src/img", to: "img" },
+        // { from: "other", to: "public" },
+      ],
     }),
   ],
 };
