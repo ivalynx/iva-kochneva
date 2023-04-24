281b5d355b507d9b1c70fdbe253837451ba0fee1
ivalynx <inkybasik@gmail.com>
2023-04-24 19:26:36 +0300

    favicon

--- webpack.common.js
+++ webpack.common.js
@@ -49,7 +49,7 @@ module.exports = {
     new CopyPlugin({
       patterns: [
         { from: "src/img", to: "img" },
-        // { from: "other", to: "public" },
+        { from: "src/favicon", to: "" },
       ],
     }),
   ],
