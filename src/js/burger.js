8eb3527b56cccce4d2501bcc183e71104f33566e
ivalynx <inkybasik@gmail.com>
2023-04-26 18:36:17 +0300

    add clickMenuItem()

--- src/js/burger.js
+++ src/js/burger.js
@@ -2,12 +2,18 @@ export default function burger() {
    
   const burgerButton = document.querySelector('.burger-button');
 
+
+
   function burgerButtonIsClikable() {   
     const burgerButtonItems = Array.from(document.querySelectorAll('.burger-button__item'));
     const burgerMenu = document.querySelector('.burger-menu');
     const burgerMenuItems = Array.from(document.querySelectorAll('.burger-menu__item'));
     const body = document.querySelector('body');
-    burgerButton.onclick = function(){
+
+    function clickMenuItem() {
+      clickBurgerButton();
+    }
+    function clickBurgerButton() {
       burgerButton.classList.toggle('burger-button_close');
       burgerButtonItems.forEach(element => {
         element.classList.toggle('burger-button_close__item');
@@ -15,9 +21,12 @@ export default function burger() {
       burgerMenu.classList.toggle('burger-menu_open');
       burgerMenuItems.forEach(element => {
         element.classList.toggle('burger-menu_open__item');
+        element.onclick = clickMenuItem;
       });
       body.classList.toggle('showOnlyHeader');
-    };
+    }
+
+    burgerButton.onclick = clickBurgerButton;
   }
 
   function checkWindowSize() {
